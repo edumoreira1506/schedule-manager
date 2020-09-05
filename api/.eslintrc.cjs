e0bd2ca41423b1bfdd70e6dd06c3ec3eebee0f45
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-09-05 20:11:26 -0300

    Add tests script

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
         'ecmaVersion': 12,
         'sourceType': 'module'
     },
+    "ignorePatterns": [ "tests/*" ],
     'rules': {
         'indent': [
             'error',
