5a0023b8ca20cb820330f3f34881c6afa65faa95
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-09-08 16:57:54 -0300

    Add admin home page

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Switch, BrowserRouter } from 'react-router-dom';
 
 import PublicRoute from './PublicRoute';
+import AdminRoute from './AdminRoute';
 import Login from '../pages/public/Login';
-import { publicRoutes } from '../config/constants';
+import AdminHome from '../pages/admin/Home';
+import { publicRoutes, adminRoutes } from '../config/constants';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <PublicRoute exact path="/" component={Login} />
       <PublicRoute exact path={publicRoutes.LOGIN} component={Login} />
+
+      <AdminRoute exact path={adminRoutes.HOME} component={AdminHome} />
     </Switch>
   </BrowserRouter>
 );
